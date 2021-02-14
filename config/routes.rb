e0cc3c2c236ed02620e53cd3a24fff3fab9fabfb
Troy Underwood <troyizzle@gmail.com>
2021-02-14 18:36:04 -0500

    update registarin controller and add username to be permitted

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root 'home#index'
   devise_for :users,
              controllers: { registrations: 'users/registrations',
-                            sessions: 'user/sessions' }
+                            sessions: 'users/sessions' }
 end
