86ed1a748a8ae86db312dbf7c0e218a6e2408942
Troy Underwood <troyizzle@gmail.com>
2021-02-14 18:45:31 -0500

    add tailwind

---
+++ app/javascript/stylesheets/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [
+    "./app/**/*.html.erb",
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.js",
+    "./app/javascript/**/*.vue",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
