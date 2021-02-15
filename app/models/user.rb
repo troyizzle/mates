031ba9e98adcde40afbb4ea0698749ecd2b16ef5
Troy Underwood <troyizzle@gmail.com>
2021-02-14 20:14:14 -0500

    add username to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :trackable
+
+  validates :username, uniqueness: true, presence: true
 end
