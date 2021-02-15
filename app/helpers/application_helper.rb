8bb9ad0302c02b259537b53b48183fc7403ee459
Troy Underwood <troyizzle@gmail.com>
2021-02-14 19:04:27 -0500

    add navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  def display_user_avatar(user, size = 40)
+    return default_image_avatar unless user.present?
+
+    return default_image_avatar
+
+    image_avatar(user.profile.avatar.variant(resize: "#{size}x#{size}!"))
+  end
+
+  private
+
+  def default_image_avatar(_size = 40)
+    image_avatar(asset_path('default.png'))
+  end
+
+  def image_avatar(img, _size = 40)
+    image_tag img, class: 'h-8 w-8 rounded-full'
+  end
 end
