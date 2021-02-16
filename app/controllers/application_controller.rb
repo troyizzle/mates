b8211335b29d8cc0d3eab3020a7a384eb6685bc9
Troy Underwood <troyizzle@gmail.com>
2021-02-15 19:57:47 -0500

    add themes to be handled from session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,24 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
+  before_action :set_theme, :user_theme
+
   default_form_builder ApplicationFormBuilder
+
+  private
+
+  def set_theme
+    session[:user_theme] = current_theme
+  end
+
+  def user_theme
+    @user_theme = session[:user_theme]
+  end
+
+  def current_theme
+    return session[:user_theme] if session[:user_theme].present?
+
+    # TODO: 'Add profile with theme type'
+    current_user ? 'dark' : 'dark'
+  end
 end
