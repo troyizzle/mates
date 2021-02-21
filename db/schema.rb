65651623c76123e9355566b4a57140adb02220c1
Troy Underwood <troyizzle@gmail.com>
2021-02-20 19:58:53 -0500

    merge profile model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_14_231044) do
+ActiveRecord::Schema.define(version: 2021_02_21_005714) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "profiles", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "theme"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -35,4 +45,5 @@ ActiveRecord::Schema.define(version: 2021_02_14_231044) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "profiles", "users"
 end
